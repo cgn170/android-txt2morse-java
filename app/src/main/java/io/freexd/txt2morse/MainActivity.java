78a03a23cc3c3d73970bf631f07c94a49744fc2f
Carlos Noguera <cgn170@gmail.com>
2017-11-24 19:31:16 -0500

    Primer commit

---
+++ app/src/main/java/io/freexd/txt2morse/MainActivity.java
@@ -0,0 +1,13 @@
+package io.freexd.txt2morse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
