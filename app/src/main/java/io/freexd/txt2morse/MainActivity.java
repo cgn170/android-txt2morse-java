01122cf4684227f18f449dd56242215c70214094
Carlos Noguera <cgn170@gmail.com>
2017-11-25 22:55:51 -0500

    Se realizaron cambios en clases para generacion de codigo morse

--- app/src/main/java/io/freexd/txt2morse/MainActivity.java
+++ app/src/main/java/io/freexd/txt2morse/MainActivity.java
@@ -1,24 +1,55 @@
 package io.freexd.txt2morse;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    MorseToFlashlight flashlight;
+    MorseToFlashlight morseToFlashlightObj;
+    Morse morseOjb;
+    EditText et_text;
+    EditText et_morse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        flashlight = new MorseToFlashlight(getApplicationContext());
+        //Se inicializan los objetos
+        morseToFlashlightObj = new MorseToFlashlight(getApplicationContext());
+        morseOjb = new Morse();
+
+        et_text = (EditText) findViewById(R.id.et_text);
+        et_morse = (EditText) findViewById(R.id.et_morse);
+
+
+
+
     }
 
 
-    public void activar_flash(View v){
-        flashlight.activarFlashLight();
+    public void morse_to_flashlight(View v){
+        morseToFlashlightObj.activarFlashLight();
     }
 
+
+    public void onClick_Convert_Text_to_Morse(View v){
+        String result=morseOjb.translateTextToMorse(et_text.getText().toString());
+        et_morse.setText(result);
+    }
+
+    public void onClick_Copy_Morse_to_Clipboard(View v) {
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Morse code", et_morse.getText().toString());
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(getApplicationContext(),"Morse code copied!",Toast.LENGTH_SHORT).show();
+
+    }
 
 }
--- app/src/main/java/io/freexd/txt2morse/Morse.java
+++ app/src/main/java/io/freexd/txt2morse/Morse.java
@@ -1,14 +1,100 @@
 package io.freexd.txt2morse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by carlos on 11/25/17.
  */
 
 public class Morse {
 
-    public Morse (){
-        
+    Map<String, String> morse_map = new HashMap<String, String>();
 
+    public Morse (){
+        morse_map.put("a",".-");
+        morse_map.put("b","-...");
+        morse_map.put("c","-.-.");
+        morse_map.put("d","-..");
+        morse_map.put("e",".");
+        morse_map.put("f","..-.");
+        morse_map.put("g","--.");
+        morse_map.put("h","....");
+        morse_map.put("i","..");
+        morse_map.put("j",".---");
+        morse_map.put("k","-.-");
+        morse_map.put("l",".-..");
+        morse_map.put("m","--");
+        morse_map.put("n","-.");
+        morse_map.put("o","---");
+        morse_map.put("p",".--.");
+        morse_map.put("q","--.-");
+        morse_map.put("r",".-.");
+        morse_map.put("s","...");
+        morse_map.put("t","-");
+        morse_map.put("u","..-");
+        morse_map.put("v","...-");
+        morse_map.put("w",".--");
+        morse_map.put("x","-..-");
+        morse_map.put("y","-.--");
+        morse_map.put("z","--..");
+        morse_map.put("1",".----");
+        morse_map.put("2","..---");
+        morse_map.put("3","...--");
+        morse_map.put("4","....-");
+        morse_map.put("5",".....");
+        morse_map.put("6","-....");
+        morse_map.put("7","--...");
+        morse_map.put("8","---..");
+        morse_map.put("9","----.");
+        morse_map.put("0","-----");
+        morse_map.put(".",".-.-.-");
+        morse_map.put(",","--..--");
+        morse_map.put("?","..--..");
+        morse_map.put("'",".----.");
+        morse_map.put("!","-.-.--");
+        morse_map.put("/","-..-.");
+        morse_map.put("(","-.--.");
+        morse_map.put(")","-.--.-");
+        morse_map.put("&",".-...");
+        morse_map.put(":","---...");
+        morse_map.put(";","-.-.-.");
+        morse_map.put("=","-...-");
+        morse_map.put("+",".-.-.");
+        morse_map.put("-","-....-");
+        morse_map.put("_","..--.-");
+        morse_map.put("\"",".-..-.");
+        morse_map.put("$","...-..-");
+        morse_map.put("end_of_work","...-.-");
+        morse_map.put("error","........");
+        morse_map.put("intivation_to_transmit","-.-");
+        morse_map.put("starting_signal","-.-.-");
+        morse_map.put("new_page_signal",".-.-.");
+        morse_map.put("understood","...-.");
+        morse_map.put("wait",".-...");
+        morse_map.put(" ","|"); // -> Vacio
+    }
+
+    //Retorna codigo morse de un string
+    public String translateTextToMorse(String text){
+        String morse="";
+        String text_lowerCase = text.toLowerCase();
+
+        for(int i=0;i<text_lowerCase.length();i++){
+            //Se comprueba que exista el caracter
+            if(morse_map.containsKey(String.valueOf(text_lowerCase.charAt(i)))){
+                //Se agrega el valor
+                morse+=morse_map.get(String.valueOf(text_lowerCase.charAt(i)));
+                //Se agrega espacio
+                morse+=" ";
+            }
+            else{ //Sino existe se agrega el signo ?
+                morse+=morse_map.get("?");
+                //Se agrega espacio
+                morse+=" ";
+            }
+        }
+        return morse;
     }
 
 }
